4dc581658270ef171170d7e81ae35fbb67a7cee2
yangfatima <yangfatima@outlook.com>
2026-09-21 02:38:55 +0800

    Add calculator functions and unit tests

---
+++ tests/test_calculator.py
@@ -0,0 +1,22 @@
+import unittest
+from src.calculator import add, subtract, divide
+
+class TestCalculator(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, -1), -2)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(1, 1), 0)
+        self.assertEqual(subtract(-1, -1), 0)
+
+    def test_divide(self):
+        self.assertEqual(divide(6, 3), 2)
+        self.assertEqual(divide(5, 2), 2.5)
+        self.assertEqual(divide(-4, 2), -2)
+
+if __name__ == '__main__':
+    unittest.main()
